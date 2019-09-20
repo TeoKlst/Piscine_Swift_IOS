7bde48a9423d7a57154eb05aff7674ec396c6002
Teo <teokelestura.tk@gmail.com>
2019-09-20 12:51:21 +0200

    20/09/19
    +Day00

---
+++ day00/Exercise00/Exercise00/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exercise00
+//
+//  Created by teo KELESTURA on 2019/09/20.
+//  Copyright © 2019 teo KELESTURA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
