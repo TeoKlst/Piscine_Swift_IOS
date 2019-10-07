a65bca2526bb20e6513ebd01e19987fc01e2e4fd
Teo <teokelestura.tk@gmail.com>
2019-10-07 14:34:47 +0200

    07/10/19
    +ex00,01 Added

---
+++ day00/ex00/ex00/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by teo KELESTURA on 2019/10/07.
+//  Copyright © 2019 teo KELESTURA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var clickmeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clickmeFunc(_ sender: Any) {
+        print("Something To Print");
+    }
+    
+}
+
---
+++ day00/ex01/ex01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ex01
+//
+//  Created by teo KELESTURA on 2019/10/07.
+//  Copyright © 2019 teo KELESTURA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var oldLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clickmeFunc(_ sender: Any) {
+        oldLabel.text = "New Label"
+    }
+    
+}
+
