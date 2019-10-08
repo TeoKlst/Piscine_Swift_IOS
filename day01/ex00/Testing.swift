234a9e086c1888a85b31ace87a71433e71e3ecdc
Teo <teokelestura.tk@gmail.com>
2019-10-08 15:49:00 +0200

    08/10/19
    +Day01
    +ex00,01

---
+++ day01/ex00/Testing.swift
@@ -0,0 +1,19 @@
+import allValues
+import allColors
+
+class test {
+    struct ApiRequest {
+        static func getColor() {
+            print(color.Color.Hearts)
+        }
+        func helloWorld(){
+            print("Hello World")
+        }
+        // for category in ProductCategory.allValues{
+        //     //Do something
+        // }
+        // for category in ProductCategory.allValues{
+        //     //Do something
+        // }
+    }
+}
