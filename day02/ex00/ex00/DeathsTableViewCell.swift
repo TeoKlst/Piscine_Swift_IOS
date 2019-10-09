e74c2b8bd220fcab8bf8eb0dde1459efb574c0c5
Teo <teokelestura.tk@gmail.com>
2019-10-09 18:00:13 +0200

    09/10/19
    +day02

---
+++ day02/ex00/ex00/DeathsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DeathsTableViewCell.swift
+//  ex00
+//
+//  Created by teo KELESTURA on 2019/10/09.
+//  Copyright © 2019 teo KELESTURA. All rights reserved.
+//
+
+import UIKit
+
+class DeathsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ day02/ex00/ex00/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by teo KELESTURA on 2019/10/09.
+//  Copyright © 2019 teo KELESTURA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
