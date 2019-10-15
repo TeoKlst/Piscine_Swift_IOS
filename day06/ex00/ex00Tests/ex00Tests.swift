7de0926174dc152827b2262fe7209fb8d109da83
Teo <teokelestura.tk@gmail.com>
2019-10-15 13:21:38 +0200

    13/10/19
    More of day6 what?

---
+++ day06/ex00/ex00Tests/ex00Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ex00Tests.swift
+//  ex00Tests
+//
+//  Created by teo KELESTURA on 2019/10/15.
+//  Copyright © 2019 teo KELESTURA. All rights reserved.
+//
+
+import XCTest
+@testable import ex00
+
+class ex00Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
